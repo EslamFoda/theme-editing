d7586d79950ca07e2793b1447770fbc06aba18c0
unknown <eslaamfodaa@gmail.com>
2022-12-06 17:26:41 +0200

    add new designs for hero section

--- components/theme/hero/index.js
+++ components/theme/hero/index.js
@@ -4,6 +4,8 @@ import Design2 from "./designs/design2";
 import Design3 from "./designs/design3";
 import Design4 from "./designs/design4";
 import Design5 from "./designs/design5";
+import Design6 from "./designs/design6";
+import Design7 from "./designs/design7";
 import ChangeSection from "../../edit/changeSection";
 import AddSection from "../../edit/addSection";
 import { updateDoc } from "firebase/firestore";
@@ -17,16 +19,18 @@ const MainHero = ({
   editSections,
   themeData,
 }) => {
-  const { compName, designNum, compData,backgroundColor } = comp;
+  const { compName, designNum, compData, backgroundColor } = comp;
   const designs = {
     design1: Design1,
     design2: Design2,
     design3: Design3,
     design4: Design4,
     design5: Design5,
+    design6: Design6,
+    design7: Design7,
   };
 
-  const HeroComp = designs[`design${designNum}`];
+  const HeroComp = designs[`design${7}`];
 
   const handleEdit = async (value, keys) => {
     const objectIndex = comps.findIndex((obj) => obj.id === comp.id);
@@ -38,10 +42,10 @@ const MainHero = ({
     });
   };
   return (
-    <div 
-    style={{
-      backgroundColor: `rgba(${backgroundColor?.r}, ${backgroundColor?.g}, ${backgroundColor?.b}, ${backgroundColor?.a})`,
-    }}
+    <div
+      style={{
+        backgroundColor: `rgba(${backgroundColor?.r}, ${backgroundColor?.g}, ${backgroundColor?.b}, ${backgroundColor?.a})`,
+      }}
       className={`relative group transition ease-in-out duration-700   ${
         editSections ? "hover:shadow-[#23cba5] hover:shadow-inside " : ""
       }  w-full `}
