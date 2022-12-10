5d63045b7c8740d1b15e5867c4d9104a24de114a
unknown <eslaamfodaa@gmail.com>
2022-12-10 14:09:15 +0200

    fix styles bugs

--- components/edit/addSection.js
+++ components/edit/addSection.js
@@ -1,19 +1,6 @@
 import { FaPlus } from "react-icons/fa";
-import { useDispatch } from "react-redux";
-import { addSectionTurnOn } from "../../features/edit-sections";
-import { selectCompName } from "../../features/comp-name";
-import { getNextIndex, selectSectionOff } from "../../features/add-section";
-import { editImgOff } from "../../features/edit-image";
-import { filesOff } from "../../features/my-files";
-import { closeColors } from "../../features/colors";
-import {
-  editEffectsOff,
-  fontEditOff,
-  stylesEditorOff,
-} from "../../features/stylesEditing";
 import { updateDoc } from "firebase/firestore";
 const AddSection = ({ index, themeData }) => {
-  const dispatch = useDispatch();
 
   return (
     <div className="absolute transition-all hidden group-hover:flex  z-10 duration-300 -bottom-21 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
@@ -27,21 +14,11 @@ const AddSection = ({ index, themeData }) => {
             editImg: false,
             addSection: true,
             editEffects: false,
-            fontEdit: true,
+            fontEdit: false,
             stylesEditing: false,
             colorsEdit: false,
-            selectSection:false
+            selectSection:false,
           });
-          // dispatch(getNextIndex(nextIndex));
-          // dispatch(selectSectionOff());
-          // dispatch(addSectionTurnOn());
-          // dispatch(selectCompName(""));
-          // dispatch(editImgOff());
-          // dispatch(filesOff());
-          // dispatch(editEffectsOff());
-          // dispatch(fontEditOff());
-          // dispatch(stylesEditorOff());
-          // dispatch(closeColors());
         }}
         class="add-section fb "
       >
